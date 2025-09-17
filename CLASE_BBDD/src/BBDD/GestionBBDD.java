2c51dc60753a448aee2fc25ddb6fc43ecf18b95d
SWG <148041802+Sandra-aki@users.noreply.github.com>
2025-09-17 23:08:53 +0200

    Delete CLASE_BBDD directory

--- CLASE_BBDD/src/BBDD/GestionBBDD.java
+++
@@ -1,9 +0,0 @@
-package BBDD;
-
-public class GestionBBDD {
-    /*
-    Metodos Public Static:
-        - return Connection conectar();
-        - void (con atributo connection que se use por los metodos.)
-    */
-}
--- CLASE_BBDD/src/DAO/MODELO/Suscripcion.java
+++
@@ -1,5 +0,0 @@
-package DAO.MODELO;
-
-public class Suscripcion {
-
-}
